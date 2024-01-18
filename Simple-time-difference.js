80f785f79fbca06080d2689e9cd5c0068c2df0c1
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-01-18 11:57:12 -0800

    Update Simple-time-difference.js

--- Simple-time-difference.js
+++ Simple-time-difference.js
@@ -17,3 +17,20 @@ For example:
 
 In the second example, the alarm sounds 4 times in a day.
 */
+
+
+function solve(arr){
+  let sortedAsMinutes = arr.map(x => timeToMinutes(x)).sort((a, b) => a - b);
+  sortedAsMinutes.push(sortedAsMinutes[0] + 60 * 24);
+  return minutesToTime(Math.max.apply(null, sortedAsMinutes.map((x, i, a) => 
+    { return i < a.length - 1 ? a[i + 1] - x : 0 })) - 1);
+}
+
+function timeToMinutes(time) {
+  let tokens = time.split(":");
+  return parseInt(tokens[0], 10) * 60 + parseInt(tokens[1], 10);
+}
+
+function minutesToTime(minutes) {
+  return ("" + Math.floor(minutes / 60)).padStart(2, "0") + ":" + ("" + minutes % 60).padStart(2, "0");
+}
